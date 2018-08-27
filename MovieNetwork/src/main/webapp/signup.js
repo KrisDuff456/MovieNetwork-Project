5cf105be98e7d670dfb07637cbce9b1e57c4f6f2
KrisDuff456 <41322885+KrisDuff456@users.noreply.github.com>
2018-08-27 18:03:13 +0100

    register/minor testing

--- MovieNetwork/src/main/webapp/signup.js
+++ MovieNetwork/src/main/webapp/signup.js
@@ -1,8 +1,16 @@
+//function signUp(first_name,last_name,email,dateOfBirth,password){
+// var xml = new XMLHttpRequest();
+//	 xml.open("POST", "http://localhost:8080/MovieNetwork/active/sign_up/addUser", true);
+//	 xml.setRequestHeader('Content-Type','application/json');
+// var data =JSON.stringify({"first_name":first_name ,"last_name":last_name, "email":email, "dateOfBirth":dateOfBirth, "password":password })
+//	 xml.send(data);
+//     xml.responseType = 'json';
+//	    
+//	console.log("Sign-Up is complete" + xml)
+//}
+
 function signUp(){
-	var xml = new XMLHttpRequest();
-	    xml.open("POST", "http://localhost:8080/MovieNetwork/signup.html", true);
-	    xml.setRequestHeader('mediatype','Application/json');
-	    xml.send();
-	    
-	console.log("User sign up information")
+//	setTimeout(function(){
+//		window.location.replace("index.html");
+//	}, 200);
 }
